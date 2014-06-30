845fd6e7ee37ff5f605de464b81666ecee560563
Nolimet <nolimet@stamdata.nl>
2014-06-30 17:04:51 +0200

    tweaked rotator
    they now never rotate all at the same speed

--- Assets/Scripts/Menu/simpleSegmentRotator.cs
+++ Assets/Scripts/Menu/simpleSegmentRotator.cs
@@ -5,6 +5,10 @@ public class simpleSegmentRotator : MonoBehaviour {
 
     [SerializeField]
     float speed = 1f;
+    void Start()
+    {
+        speed = ((speed*0.25f) * Random.value) + (speed*0.75f);
+    }
 	void Update () {
         transform.Rotate(0, 0, speed * Time.deltaTime); 
 	}
