5f46f8ee6becfb6364fc40ab35bf8c17c49ab1ee
Nolimet <nolimet@stamdata.nl>
2014-06-30 16:53:40 +0200

    added new menu elements
    added a new object at mainmenu
    added exit button to menu -_-
    changed location of a few assets
    tweaked a few items

---
+++ Assets/Scripts/Menu/simpleSegmentRotator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class simpleSegmentRotator : MonoBehaviour {
+
+    [SerializeField]
+    float speed = 1f;
+	void Update () {
+        transform.Rotate(0, 0, speed * Time.deltaTime); 
+	}
+}
