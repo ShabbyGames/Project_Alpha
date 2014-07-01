04fede63379e90a7b42ea7fc2c5987946bb8a798
Nolimet <nolimet@stamdata.nl>
2014-07-02 00:49:05 +0200

    Made menu background a bit cooler
    converting circle to psd to PNG soon

--- Assets/Scripts/Menu/simpleSegmentRotator.cs
+++ Assets/Scripts/Menu/simpleSegmentRotator.cs
@@ -4,12 +4,31 @@ using System.Collections;
 public class simpleSegmentRotator : MonoBehaviour {
 
     [SerializeField]
-    float speed = 1f;
+    float DegreesPerSecond = 1f;
+    float orignalSpeed;
+    int direction = -1;
+
+    float timer = 0f;
     void Start()
     {
-        speed = ((speed*0.25f) * Random.value) + (speed*0.75f);
+        orignalSpeed = DegreesPerSecond;
+        DegreesPerSecond = ((DegreesPerSecond*0.25f) * Random.value) + (DegreesPerSecond*0.75f);
     }
 	void Update () {
-        transform.Rotate(0, 0, speed * Time.deltaTime); 
+        if (DegreesPerSecond >= orignalSpeed && direction == 1 || DegreesPerSecond <= -orignalSpeed && direction == -1)
+            timer += Time.deltaTime;
+        else
+            DegreesPerSecond += ((orignalSpeed * direction) / 3f)*Time.deltaTime;
+
+
+        if (timer > 1f)
+        {
+            timer = 0f;
+            direction = direction * -1;
+        }
+
+        float step = DegreesPerSecond * Time.deltaTime;
+        transform.Rotate(0, 0, step);
+       
 	}
 }
