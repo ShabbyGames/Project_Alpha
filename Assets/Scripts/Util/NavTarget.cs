83f08b0d3a462a4b77ba9073f438039f6ff73cea
Nolimet <nolimet@stamdata.nl>
2014-11-12 19:01:55 +0100

    tweak tweak test test

--- Assets/Scripts/Util/NavTarget.cs
+++ Assets/Scripts/Util/NavTarget.cs
@@ -19,16 +19,8 @@ public class NavTarget : MonoBehaviour {
         {
             RaycastHit hit;
             if (Physics.Raycast(transform.position, target.position - transform.position, out hit, 100))
-            {
-                print(hit.collider.gameObject.tag);
-
                 if (hit.transform.gameObject.tag == "Player")
-                {
                     agent.SetDestination(target.position);
-                    print("boom");
-                }
-            }
         }
-        Debug.DrawRay(transform.position, target.position-transform.position, Color.red, 1f);
 	}
 }
